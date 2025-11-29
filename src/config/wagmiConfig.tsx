3bc19a050be6cca4fb414683611001b5bec8f53d
Maffaz.Eth <johnashu1@hotmail.com>
2025-11-29 13:40:29 +0000

    update USDC env

--- src/config/wagmiConfig.tsx
+++ src/config/wagmiConfig.tsx
@@ -151,7 +151,7 @@ export const chainConfig: Record<number, CustomChainConfig> = {
       bgColor: 'bg-[#0051ff]',
       textColor: 'text-[#0051ff]',
       tokens: {
-         USDC: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'
+         USDC: process.env.NEXT_PUBLIC_BASE_USDC_ADDRESS || '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'
       }
    },
    // [arbitrum.id]: {
@@ -202,7 +202,7 @@ export const chainConfig: Record<number, CustomChainConfig> = {
       bgColor: 'bg-[#0051ff]',
       textColor: 'text-[#0051ff]',
       tokens: {
-         USDC: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
+         USDC: process.env.NEXT_PUBLIC_BASE_SEPOLIA_USDC_ADDRESS || '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
          USDT: '0x73b4a58138cccbda822df9449feda5eac6669ebd'
       }
    }
