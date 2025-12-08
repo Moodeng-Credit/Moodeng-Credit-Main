18b6e35d5b55777fb78c0ee526c16d6ad26cdd3e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-12-08 09:55:23 +0700

    fix: adjust gap and padding

--- src/views/support/components/Section.tsx
+++ src/views/support/components/Section.tsx
@@ -9,7 +9,7 @@ export default function Section(props: { header: ReactNode; content: ReactNode; 
             'bg-[#c9d5f9]': props.bg
          })}
       >
-         <div className="flex flex-col lg:flex-row justify-between items-start md:items-center gap-4 pe-16">
+         <div className="flex flex-col lg:flex-row justify-between items-start md:items-center gap-4 sm:gap-8 lg:pe-16">
             <h2 className="text-5xl font-bold text-blue-600">{props.header}</h2>
             <div className="max-w-lg flex flex-col gap-8 w-full text-xl">{props.content}</div>
          </div>
--- src/views/support/sections/ContactUs.tsx
+++ src/views/support/sections/ContactUs.tsx
@@ -1,7 +1,7 @@
 export default function ContactUs() {
    return (
       <div className="bg-[#c9d5f9] flex flex-col px-6 md:px-20 py-16 min-h-64 justify-center">
-         <div className="flex flex-col lg:flex-row justify-between items-start md:items-center gap-4 pe-16">
+         <div className="flex flex-col lg:flex-row justify-between items-start md:items-center gap-4 sm:gap-8 lg:pe-16">
             <h2 className="text-6xl font-bold text-blue-600">Contact Us</h2>
             <div className="max-w-lg text-xl">
                <p>Have questions? The quickest way to get in touch with us is using the contact information below.</p>
