62067e5096ccd5a8d65cd45f985b882989fc4650
MrRexZ <anthonytjuatja@gmail.com>
2026-01-06 13:58:53 +0700

    chore: make app accessible via network to help local testing via mobile + made app hosted via HTTPS to allow base wallet test

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 // Force re-bundle
 export default defineConfig({
-   plugins: [react()],
+   plugins: [react(), mkcert()],
    resolve: {
       alias: {
          '@': path.resolve(__dirname, './src')
@@ -13,6 +14,7 @@ export default defineConfig({
    },
    server: {
       port: 3000,
+      host: true, // Listen on all network interfaces
       allowedHosts: ['.ngrok-free.app', '.ngrok.io']
    }
 });
