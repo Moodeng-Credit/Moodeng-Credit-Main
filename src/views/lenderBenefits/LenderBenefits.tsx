92557518e56139be0ebaaa5b5b36c73a2488b4ac
moodeng <moodeng@example.com>
2026-05-05 10:45:16 +0700

    Refresh benefits and why lend pages

--- src/views/lenderBenefits/LenderBenefits.tsx
+++ src/views/lenderBenefits/LenderBenefits.tsx
@@ -1,8 +1,5 @@
-
-
 import { useEffect } from 'react';
 
-import StartBuildingCreditSection from '@/views/borrowerBenefits/sections/StartBuildingCreditSection';
 import CommunityHeroSection from '@/views/lenderBenefits/sections/CommunityHeroSection';
 import FeaturesSection from '@/views/lenderBenefits/sections/FeaturesSection';
 import HowWeVerifySection from '@/views/lenderBenefits/sections/HowWeVerifySection';
@@ -13,6 +10,7 @@ import ProblemSection from '@/views/lenderBenefits/sections/ProblemSection';
 import VerfiyStatsSection from '@/views/lenderBenefits/sections/VerfiyStatsSection';
 import VerifyIdentitySection from '@/views/lenderBenefits/sections/VerifyIdentitySection';
 import Web3WalletSection from '@/views/lenderBenefits/sections/Web3WalletSection';
+import '@/views/lenderBenefits/styles/LenderBenefits.css';
 
 export default function LenderBenefits() {
    useEffect(() => {
@@ -26,28 +24,31 @@ export default function LenderBenefits() {
 
    return (
       <>
-         <div id="why-lend" className="flex overflow-hidden flex-col bg-zinc-900">
+         <div id="why-lend" className="flex min-h-screen flex-col overflow-x-hidden bg-zinc-900">
             <div className="flex flex-col items-center w-full min-h-full max-md:max-w-full">
                <CommunityHeroSection />
 
-               <div id="problem" className="flex relative flex-col items-center mt-20 max-w-full w-[1064px] max-md:mt-10">
+               <section
+                  id="problem"
+                  className="lender-story-section flex relative flex-col items-center mt-16 max-w-full w-[1064px] max-md:mt-0"
+               >
                   <ProblemSection />
                   <Web3WalletSection />
                   <LendingIncentivesSection />
-               </div>
+               </section>
 
-               <div className="flex flex-col pt-10 mt-20 max-w-full rounded-none w-[1440px] max-md:mt-10">
+               <section className="lender-borrowers-section flex flex-col pt-10 mt-16 max-w-full rounded-none w-[1440px] max-md:mt-0">
                   <MeetYourFutureBorrowersSection />
                   <MostNeededSection />
-               </div>
+               </section>
 
-               <HowWeVerifySection />
-               <VerfiyStatsSection />
-               <VerifyIdentitySection />
+               <section className="lender-verify-section flex flex-col items-center w-full">
+                  <HowWeVerifySection />
+                  <VerfiyStatsSection />
+                  <VerifyIdentitySection />
+               </section>
 
                <FeaturesSection />
-
-               <StartBuildingCreditSection />
             </div>
          </div>
       </>
